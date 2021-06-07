b8c8151055f1e9187e112b6a34c5f7306951a24d
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2021-06-07 22:59:46 +0800

    order form and view the result

--- app/src/main/java/com/irfanshukri203/checkboxandradiobuttontutorial/MainActivity.kt
+++ app/src/main/java/com/irfanshukri203/checkboxandradiobuttontutorial/MainActivity.kt
@@ -2,10 +2,27 @@ package com.irfanshukri203.checkboxandradiobuttontutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RadioButton
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnOrder.setOnClickListener {
+            val checkedMeatRadioButtonId = rgMeat.checkedRadioButtonId
+            val meet = findViewById<RadioButton>(checkedMeatRadioButtonId)
+            val cheese = cbCheese.isChecked
+            val onions = cbOnion.isChecked
+            val salad = cbSalad.isChecked
+            val orderString = "You've ordered burger with:\n" +
+                    "${meet.text}" +
+                    (if(cheese) "\nCheese" else "") +
+                    (if(onions) "\nOnions" else "") +
+                    (if(salad) "\nSalad" else "")
+
+            tvOrder.text = orderString.toString()
+        }
     }
 }
